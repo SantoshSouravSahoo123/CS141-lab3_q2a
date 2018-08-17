d26ec80ab311ec17db0900100b2b2f1f6cf5b658
SantoshSouravSahoo123 <42238175+SantoshSouravSahoo123@users.noreply.github.com>
2018-08-17 05:26:30 +0000

    Add files via upload

---
+++ lab3_q2d.cpp
@@ -0,0 +1,18 @@
+//initialization of variables
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a=55;		// initial value = 55
+	int b(2);		// initial value = 2
+	int result;		// initial value
+//undetermined
+
+	a = a + 5;
+	result = a % b;
+	cout << result;
+
+	return 0;
+}
