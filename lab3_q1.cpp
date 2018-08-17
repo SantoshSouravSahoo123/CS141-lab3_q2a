e83c39faa20bfa04e50fc8de0095808e5b370f21
SantoshSouravSahoo123 <42238175+SantoshSouravSahoo123@users.noreply.github.com>
2018-08-17 06:02:12 +0000

    Add files via upload

--- lab3_q1.cpp
+++ lab3_q1.cpp
@@ -1,23 +1,13 @@
-// operating with variables
-
 #include<iostream>
 using namespace std;
-
-int main()
-{
-	// declaring variables:
-	int a, b;
-	int result;
-
-	//process:
-	a = 5;
-	b = 2;
-	a = a + 1;
-	result = a - b;
-
-	// print out the result:
-	cout << result;
-
-	// terminate the program:
-	return 0;
+int main(){
+int a;
+float b;
+bool t;
+double e;
+cout <<"size of a is "<<sizeof(a)<<"\n";
+cout <<"size of b is "<<sizeof(b)<<"\n";
+cout <<"size of t is "<<sizeof(t)<<"\n";
+cout <<"size of e is "<<sizeof(e)<<"\n";
+return 0;
 }
